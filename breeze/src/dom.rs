267cd1380280d447572a2853e4d203aea6cfa188
griffi-gh <prasol258@gmail.com>
2023-08-17 02:26:09 +0200

    parse attributes

--- breeze/src/dom.rs
+++ breeze/src/dom.rs
@@ -146,34 +146,69 @@ impl Dom {
     fn parse_tree(parent_node: &SharedNode, tree_thingy: Pair<HtmlRule>) {
       assert_eq!(tree_thingy.as_rule(), HtmlRule::tree);
       for node_thingy in tree_thingy.into_inner() {
+
         assert_eq!(node_thingy.as_rule(), HtmlRule::node);
         let node_subtype = node_thingy.into_inner().next().unwrap();
         let node_subtype_rule = node_subtype.as_rule();
+
         match node_subtype_rule {
+          //Full tags: <b>...</b>, and void tags: <img>, <div/>
           HtmlRule::full_tag | HtmlRule::void_tag => {
             let mut node_subtype_inner = node_subtype.into_inner();
 
-            let opening_tag = node_subtype_inner.next().unwrap();
+            let opening_tag_or_ident = node_subtype_inner.next().unwrap();
 
             //parse opening tag (get name and attributes)
             let (tag_name, attributes) = {
-              let tag_name = match opening_tag.as_rule() {
+              let (attributes_pairs, tag_name) = match opening_tag_or_ident.as_rule() {
                 HtmlRule::opening_tag => {
-                  let mut opening_tag_inner = opening_tag.into_inner();
+                  let mut opening_tag_inner = opening_tag_or_ident.into_inner();
                   let tag_name_pair = opening_tag_inner.next().unwrap();
                   assert_eq!(tag_name_pair.as_rule(), HtmlRule::ident);
-                  tag_name_pair.as_str().into()
+                  let tag_name = tag_name_pair.as_str().into();
+                  let attributes_pair = opening_tag_inner.next();
+                  (attributes_pair, tag_name)
                 },
                 HtmlRule::ident | HtmlRule::void_tag_name => {
-                  opening_tag.as_str().into()
+                  let tag_name = opening_tag_or_ident.as_str().into();
+                  let attributes_pair = node_subtype_inner.next();
+                  (attributes_pair, tag_name)
                 },
                 _ => unreachable!()
               };
-              //TODO parse attributes
-              let attributes = FxHashMap::default();
+              
+              //parse attributes
+              let mut attributes = FxHashMap::default();
+              if let Some(attributes_pairs) = attributes_pairs {
+                for attribute_pair in attributes_pairs.into_inner() {
+                  assert_eq!(attribute_pair.as_rule(), HtmlRule::attribute);
+                  let mut attribute_inner = attribute_pair.into_inner();
+
+                  let attribute_ident = attribute_inner.next().unwrap();
+                  assert_eq!(attribute_ident.as_rule(), HtmlRule::ident);
+                  let attribute_name = attribute_ident.as_str().to_owned();
+
+                  let attribute_value: String = if let Some(str_pair) = attribute_inner.next() {
+                    match str_pair.as_rule() {
+                      HtmlRule::value_proper => {
+                        let mut as_str = &str_pair.as_str()[1..];
+                        as_str = &as_str[..(as_str.len() - 1)];
+                        as_str.replace("\\\\", "\\")
+                      },
+                      HtmlRule::value_naked => str_pair.as_str().into(),
+                      _ => unreachable!()
+                    }
+                  } else {
+                    "".into()
+                  };
+
+                  attributes.insert(attribute_name, attribute_value);
+                }
+              }
               (tag_name, attributes)
             };
             
+            //Create and insert new node
             let new_node: SharedNode = Node::Element(ElementNode {
               tag_name, attributes,
               ..Default::default()
@@ -181,6 +216,7 @@ impl Dom {
             println!("NODE {:?}", new_node);
             parent_node.push(new_node.shared_clone()).unwrap();
 
+            //If full tag, recursively parse children
             if node_subtype_rule == HtmlRule::full_tag {
               let ptree = node_subtype_inner.next().unwrap();
               assert!(ptree.as_rule() == HtmlRule::tree);
@@ -191,6 +227,8 @@ impl Dom {
             // let closing_tag = node_subtype_inner.next().unwrap();
             // assert!(closing_tag.as_rule() == HtmlRule::closing_tag);
           },
+
+          //Text nodes
           HtmlRule::text_node => {
             let new_node: SharedNode = Node::Text(TextNode {
               text: node_subtype.as_str().into(),
@@ -199,6 +237,7 @@ impl Dom {
             println!("TEXT {:?}", new_node);
             parent_node.push(new_node).unwrap();
           },
+
           _ => unreachable!(),
         }
       }

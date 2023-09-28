5a730f3fa32205e48d5078738d9292d78ebb3111
griffi-gh <prasol258@gmail.com>
2023-09-28 22:57:34 +0200

    aaa

--- breeze/src/elements.rs
+++ breeze/src/elements.rs
@@ -3,26 +3,33 @@ use nalgebra::Vector2;
 
 ///Internal function\
 ///Panics if node is a Text node
-fn element_node(node: &SharedNode) -> &ElementNode {
-  match &*node.0.borrow() {
+fn element_node<'a>(node: &'a SharedNode) -> &'a ElementNode {
+  let x = node.0.borrow();
+  match &*x {
     Node::Element(node) => node,
     _ => unreachable!()
   }
 }
 
 pub trait ElementInterface {
-  
-
   /// Compute node's internal content size\
-  /// Defaults to a 10x10 square if no children\
+  /// 
+  /// Defaults to a 10x10 square (if no children)\
+  /// You may want to change that, for example, for images you should return the image size here.
+  /// 
   /// Should never get called for text nodes\
   /// (they cant have ElementInterface anyway, 
-  /// if you need to do that you're doing sth *very* wrong)
+  /// if you need to do that you're doing sth *very* wrong)\
+  /// 
+  /// if regular box layout w/children, should return None
+  /// 
+  /// not guaranteed to match the actual size after the layout stage.\
+  /// this is not exact and it can and will be overriden by styles.
   fn compute_content_size(&self, node: &SharedNode) -> Option<Vector2<f32>> {
     element_node(node).children.is_empty().then_some(Vector2::new(10., 10.))
   }
 
-   //compute_layout_
+  //compute_layout_
 }
 
 pub struct DefaultNode;
